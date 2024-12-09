503dfff60d1b37d94aa45ade2e7967fa853bb06a
Natalie Cuthbert <natalie@wbbl.co>
2024-12-09 09:18:32 +0200

    Safety commit

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -7,21 +7,27 @@ using UnityEngine.VFX;
 public class Rope : MonoBehaviour
 {
     private static readonly int PreviousPositions = Shader.PropertyToID("PreviousPositions");
+    private static readonly int PositionErrors = Shader.PropertyToID("PositionErrors");
 
     [field: SerializeField] public VisualEffect RopeEffect { get; private set; }
-    private GraphicsBuffer _previousPositionsBuffer;
-    
+    private GraphicsBuffer _positionsBuffer;
+    private GraphicsBuffer _positionErrorBuffer;
+
     // Start is called before the first frame update
     void Start()
     {
-        _previousPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 4 * sizeof(float));
-        _previousPositionsBuffer.SetData(new Vector4[32]);
-        RopeEffect.SetGraphicsBuffer(PreviousPositions, _previousPositionsBuffer);
+        _positionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(float));
+        _positionsBuffer.SetData(new Vector3[32]);
+        _positionErrorBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(float));
+        _positionErrorBuffer.SetData(new Vector3[32]);
+        RopeEffect.SetGraphicsBuffer(PreviousPositions, _positionsBuffer);
+        RopeEffect.SetGraphicsBuffer(PositionErrors, _positionErrorBuffer);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        RopeEffect.SetGraphicsBuffer(PreviousPositions, _previousPositionsBuffer);
+        RopeEffect.SetGraphicsBuffer(PreviousPositions, _positionsBuffer);
+        RopeEffect.SetGraphicsBuffer(PositionErrors, _positionErrorBuffer);
     }
 }
--- Assets/Shaders/RopeFuncs.hlsl
+++ Assets/Shaders/RopeFuncs.hlsl
@@ -1,54 +1,60 @@
-void WriteBuffer(VFXAttributes attributes, RWStructuredBuffer<float4> buffer, uint index, float4 inPosition) {
-	buffer[index] = inPosition;
+void WriteBuffer(VFXAttributes attributes, RWStructuredBuffer<float3> buffer, uint index, float3 value) {
+    float _x,_y,_z = 0;
+    InterlockedExchange(buffer[index].x, value.x, _x);
+    InterlockedExchange(buffer[index].y, value.y, _y);
+    InterlockedExchange(buffer[index].z, value.z, _z);
 }
 
-float4 SafeNormalize(float4 inVec)
+void ExchangeBuffer(RWStructuredBuffer<float3> buffer, uint index, float3 current, out float3 prev) {
+    float _x,_y,_z = 0;
+    InterlockedExchange(buffer[index].x, current.x, prev.x);
+    InterlockedExchange(buffer[index].y, current.y, prev.y);
+    InterlockedExchange(buffer[index].z, current.z, prev.z);
+}
+
+
+void IntegrateVerlet(inout VFXAttributes attributes, float deltaTime)
 {
-    float dp3 = max(0.000001, dot(inVec, inVec));
-    return inVec * rsqrt(dp3);
+    attributes.position = 2 * attributes.position - attributes.oldPosition + ((deltaTime*deltaTime) * attributes.acceleration);
 }
 
-void ApplyConstraint(inout VFXAttributes attributes, RWStructuredBuffer<float4> buffer, float targetDist, uint bufferSize, uint currIndex)
+void ApplyConstraint(inout VFXAttributes attributes, RWStructuredBuffer<float3> positionsBuffer, RWStructuredBuffer<float3> errorBuffer, float targetDist, uint bufferSize, uint currIndex)
 {   
-    buffer[currIndex] = float4(attributes.position, 0.0);
-    if (currIndex % 2 == 1)
+    float3 thisPosition = attributes.position;
+    float3 nextThisPosition = attributes.position;
+    if (currIndex > 0)
     {
         [unroll(48)]
         for (uint i = 0; i < 48; ++i)
         {
-            float4 current = buffer[currIndex];
-            uint index = clamp(currIndex - 1, 0, bufferSize);
-            float4 position = buffer[index];
-            float4 delta = (position - current);
+            ExchangeBuffer(positionsBuffer, currIndex, nextThisPosition, thisPosition);
+            uint index = currIndex - 1;
+            float3 position = positionsBuffer[index];
+            float3 delta = (position - current);
             float dist = length(delta);
-            float halfDist = (dist - targetDist) / 2.0;
-            if (currIndex > 0)
+            float halfDist = (dist - targetDist) / 2.0;                
+            delta = SafeNormalize(delta);
+            if (currIndex == bufferSize - 1)
             {
-                delta = SafeNormalize(delta);
-                if (currIndex == bufferSize - 1)
-                {
-                    buffer[currIndex] += ((halfDist + halfDist) * delta);
-                }
-                else
-                {
-                    buffer[currIndex] += (halfDist * delta);
-                    buffer[index] -= (halfDist * delta);
-                }
+                next += ((halfDist + halfDist) * delta);
             }
-        
+            else
+            {
+                next += (halfDist * delta);
+            }
+            
             if (currIndex + 1 < bufferSize)
             {
+                current = positionsBuffer[currIndex];
                 index = currIndex + 1;
-                position = buffer[index];
+                position = positionsBuffer[index];
                 delta = (position - current);
                 dist = length(delta);
                 halfDist = (dist - targetDist) / 2.0;
                 delta = SafeNormalize(delta);
-                buffer[currIndex] += (halfDist * delta);
-                buffer[index] -= (halfDist * delta);
+                next += (halfDist * delta);
             }
         }
+        attributes.position =  next;
     }
-
-    attributes.position = buffer[currIndex].xyz;
 }
