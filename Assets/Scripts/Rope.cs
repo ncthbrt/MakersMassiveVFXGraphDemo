524b1ba13afeb0805da02dde8760363b4c766fc9
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-12-15 15:20:51 +0200

    Attempting to use interlockedadd for mutexes

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -6,28 +6,26 @@ using UnityEngine.VFX;
 [ExecuteInEditMode]
 public class Rope : MonoBehaviour
 {
-    private static readonly int PreviousPositions = Shader.PropertyToID("PreviousPositions");
-    private static readonly int PositionErrors = Shader.PropertyToID("PositionErrors");
+    private static readonly int Positions = Shader.PropertyToID("Positions");
+    private static readonly int Mutexes = Shader.PropertyToID("Mutexes");
 
     [field: SerializeField] public VisualEffect RopeEffect { get; private set; }
-    private GraphicsBuffer _positionsBuffer;
-    private GraphicsBuffer _positionErrorBuffer;
+    private GraphicsBuffer _previousPositionsBuffer;
+    private GraphicsBuffer _mutexBuffer;
 
-    // Start is called before the first frame update
     void Start()
     {
-        _positionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(float));
-        _positionsBuffer.SetData(new Vector3[32]);
-        _positionErrorBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(float));
-        _positionErrorBuffer.SetData(new Vector3[32]);
-        RopeEffect.SetGraphicsBuffer(PreviousPositions, _positionsBuffer);
-        RopeEffect.SetGraphicsBuffer(PositionErrors, _positionErrorBuffer);
+        _previousPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(uint));
+        _previousPositionsBuffer.SetData(new Vector3[32]);
+        _mutexBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, sizeof(uint));
+        _mutexBuffer.SetData(new uint[32]);
+        RopeEffect.SetGraphicsBuffer(Positions, _previousPositionsBuffer);
+        RopeEffect.SetGraphicsBuffer(Mutexes, _mutexBuffer);
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        RopeEffect.SetGraphicsBuffer(PreviousPositions, _positionsBuffer);
-        RopeEffect.SetGraphicsBuffer(PositionErrors, _positionErrorBuffer);
+        RopeEffect.SetGraphicsBuffer(Positions, _previousPositionsBuffer);
+        RopeEffect.SetGraphicsBuffer(Mutexes, _mutexBuffer);
     }
 }
--- Assets/Shaders/RopeFuncs.hlsl
+++ Assets/Shaders/RopeFuncs.hlsl
@@ -1,60 +1,108 @@
-void WriteBuffer(VFXAttributes attributes, RWStructuredBuffer<float3> buffer, uint index, float3 value) {
-    float _x,_y,_z = 0;
-    InterlockedExchange(buffer[index].x, value.x, _x);
-    InterlockedExchange(buffer[index].y, value.y, _y);
-    InterlockedExchange(buffer[index].z, value.z, _z);
-}
-
-void ExchangeBuffer(RWStructuredBuffer<float3> buffer, uint index, float3 current, out float3 prev) {
-    float _x,_y,_z = 0;
-    InterlockedExchange(buffer[index].x, current.x, prev.x);
-    InterlockedExchange(buffer[index].y, current.y, prev.y);
-    InterlockedExchange(buffer[index].z, current.z, prev.z);
-}
-
-
-void IntegrateVerlet(inout VFXAttributes attributes, float deltaTime)
+#ifndef ROPE_FUNCS
+#define ROPE_FUNCS
+// Check isnan(value) before use.
+uint OrderPreservingFloatMap(float value)
 {
-    attributes.position = 2 * attributes.position - attributes.oldPosition + ((deltaTime*deltaTime) * attributes.acceleration);
+    // For negative values, the mask becomes 0xffffffff.
+    // For positive values, the mask becomes 0x80000000.
+    uint uvalue = asuint(value);
+    uint mask = -int(uvalue >> 31) | 0x80000000;
+    return uvalue ^ mask;
 }
 
-void ApplyConstraint(inout VFXAttributes attributes, RWStructuredBuffer<float3> positionsBuffer, RWStructuredBuffer<float3> errorBuffer, float targetDist, uint bufferSize, uint currIndex)
+float InverseOrderPreservingFloatMap(uint value)
+{
+    // If the msb is set, the mask becomes 0x80000000.
+    // If the msb is unset, the mask becomes 0xffffffff.
+    uint mask = ((value >> 31) - 1) | 0x80000000;
+    return asfloat(value ^ mask);
+}
+
+void AtomicWriteBuffer(RWStructuredBuffer<uint3> positions, uint index, float3 inPosition) {
+    uint3 _ignored = uint3(0, 0, 0);
+    InterlockedExchange(positions[index].x, OrderPreservingFloatMap(inPosition.x), _ignored.x);
+    InterlockedExchange(positions[index].y, OrderPreservingFloatMap(inPosition.y), _ignored.y);
+    InterlockedExchange(positions[index].z, OrderPreservingFloatMap(inPosition.z), _ignored.z);
+}
+
+void WriteBuffer(VFXAttributes attributes, RWStructuredBuffer<uint3> positions, uint index) {    
+	AtomicWriteBuffer(positions, index, attributes.position);
+}
+
+void AtomicLoadBuffer(RWStructuredBuffer<uint3> positions, uint index, out float3 outPosition) {
+    uint3 position = uint3(0, 0, 0);
+    InterlockedAdd(positions[index].x, 0, position.x);
+    InterlockedAdd(positions[index].y, 0, position.y);
+    InterlockedAdd(positions[index].z, 0, position.z);
+    outPosition = float3(InverseOrderPreservingFloatMap(position.x), InverseOrderPreservingFloatMap(position.y), InverseOrderPreservingFloatMap(position.z));    
+}
+
+void UpdateRope(inout VFXAttributes attributes, RWStructuredBuffer<uint3> positions, RWStructuredBuffer<uint> mutexes, float targetDist, uint bufferSize, uint currIndex)
 {   
-    float3 thisPosition = attributes.position;
-    float3 nextThisPosition = attributes.position;
-    if (currIndex > 0)
-    {
-        [unroll(48)]
-        for (uint i = 0; i < 48; ++i)
+    uint ignored = 0;
+    uint mutex = 0;
+    AtomicWriteBuffer(positions, currIndex, attributes.position);
+    InterlockedExchange(mutexes[currIndex], currIndex + 1, ignored);
+
+    for (uint i = 0; i < 48; ++i)
+    {   
+        if (currIndex > 0) 
         {
-            ExchangeBuffer(positionsBuffer, currIndex, nextThisPosition, thisPosition);
-            uint index = currIndex - 1;
-            float3 position = positionsBuffer[index];
-            float3 delta = (position - current);
-            float dist = length(delta);
-            float halfDist = (dist - targetDist) / 2.0;                
-            delta = SafeNormalize(delta);
-            if (currIndex == bufferSize - 1)
+            for(uint j=0; j<10; ++j)
             {
-                next += ((halfDist + halfDist) * delta);
-            }
-            else
-            {
-                next += (halfDist * delta);
-            }
-            
-            if (currIndex + 1 < bufferSize)
-            {
-                current = positionsBuffer[currIndex];
-                index = currIndex + 1;
-                position = positionsBuffer[index];
-                delta = (position - current);
-                dist = length(delta);
-                halfDist = (dist - targetDist) / 2.0;
-                delta = SafeNormalize(delta);
-                next += (halfDist * delta);
+                InterlockedAdd(mutexes[currIndex - 1], 0, mutex);
+                if(mutex == currIndex)
+                {
+                    float3 current = float3(0.0, 0.0, 0.0);
+                    float3 other = float3(0.0, 0.0, 0.0);
+                    AtomicLoadBuffer(positions, currIndex, current);
+                    // AtomicLoadBuffer(positions, currIndex - 1, other);
+                    
+                    // float3 delta = (current - other);
+                    // float dist = length(delta);
+                    // float halfDist = (dist - targetDist) / 2.0;
+                    // delta = SafeNormalize(delta);
+                    // if (currIndex == bufferSize - 1)
+                    // {
+                    //     AtomicWriteBuffer(positions, currIndex, current + ((halfDist + halfDist) * delta));
+                    // }
+                    // else
+                    // {
+                    //     AtomicWriteBuffer(positions, currIndex, current + (halfDist * delta));
+                    //     AtomicWriteBuffer(positions, currIndex - 1, other - (halfDist * delta));
+                    // }
+                    
+                    InterlockedExchange(mutexes[currIndex - 1], currIndex - 1, ignored);
+                    break;
+                }
             }
         }
-        attributes.position =  next;
+        
+        if (currIndex + 1 < bufferSize)
+        {
+            for(uint j=0; j<10; ++j) 
+            {
+                InterlockedAdd(mutexes[currIndex], 0, mutex);
+                if(mutex == currIndex)
+                {
+                    // float3 current = float3(0.0, 0.0, 0.0);
+                    // float3 other = float3(0.0, 0.0, 0.0);
+                    // AtomicLoadBuffer(positions, currIndex, current);
+                    // AtomicLoadBuffer(positions, currIndex + 1, other);                    
+                    // float3 delta = (current - other);
+                    // float dist = length(delta);
+                    // float halfDist = (dist - targetDist) / 2.0;
+                    // delta = SafeNormalize(delta);
+                  
+                    // AtomicWriteBuffer(positions, currIndex, current + (halfDist * delta));
+                    // AtomicWriteBuffer(positions, currIndex + 1, other - (halfDist * delta));
+                    // InterlockedExchange(mutexes[currIndex], currIndex + 1, ignored);
+                    // break;
+                }
+            }
+        }
     }
+
+    AtomicLoadBuffer(positions, currIndex, attributes.position);
 }
+#endif
