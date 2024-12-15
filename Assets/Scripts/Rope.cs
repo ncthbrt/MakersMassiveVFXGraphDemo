17848f7cc9297648cb41c57b54596270a9469a02
Natalie Cuthbert <natalie@cuthbert.co.za>
2024-12-16 00:08:24 +0200

    Making progress

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -15,10 +15,10 @@ public class Rope : MonoBehaviour
 
     void Start()
     {
-        _previousPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, 3 * sizeof(uint));
-        _previousPositionsBuffer.SetData(new Vector3[32]);
-        _mutexBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32, sizeof(uint));
-        _mutexBuffer.SetData(new uint[32]);
+        _previousPositionsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Raw, 32, 3 * sizeof(uint));
+        _previousPositionsBuffer.SetData(new byte[3* sizeof(uint) * 32]);
+        _mutexBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Raw, 32, sizeof(uint));
+        _mutexBuffer.SetData(new byte[sizeof(uint) * 32]);
         RopeEffect.SetGraphicsBuffer(Positions, _previousPositionsBuffer);
         RopeEffect.SetGraphicsBuffer(Mutexes, _mutexBuffer);
     }
--- Assets/Shaders/RopeFuncs.hlsl
+++ Assets/Shaders/RopeFuncs.hlsl
@@ -1,7 +1,7 @@
 #ifndef ROPE_FUNCS
 #define ROPE_FUNCS
 // Check isnan(value) before use.
-uint OrderPreservingFloatMap(float value)
+inline uint OrderPreservingFloatMap(float value)
 {
     // For negative values, the mask becomes 0xffffffff.
     // For positive values, the mask becomes 0x80000000.
@@ -10,7 +10,7 @@ uint OrderPreservingFloatMap(float value)
     return uvalue ^ mask;
 }
 
-float InverseOrderPreservingFloatMap(uint value)
+inline float InverseOrderPreservingFloatMap(uint value)
 {
     // If the msb is set, the mask becomes 0x80000000.
     // If the msb is unset, the mask becomes 0xffffffff.
@@ -18,91 +18,118 @@ float InverseOrderPreservingFloatMap(uint value)
     return asfloat(value ^ mask);
 }
 
-void AtomicWriteBuffer(RWStructuredBuffer<uint3> positions, uint index, float3 inPosition) {
-    uint3 _ignored = uint3(0, 0, 0);
-    InterlockedExchange(positions[index].x, OrderPreservingFloatMap(inPosition.x), _ignored.x);
-    InterlockedExchange(positions[index].y, OrderPreservingFloatMap(inPosition.y), _ignored.y);
-    InterlockedExchange(positions[index].z, OrderPreservingFloatMap(inPosition.z), _ignored.z);
+inline uint3 Float3ToUint3(float3 value)
+{
+    return uint3(OrderPreservingFloatMap(value.x),OrderPreservingFloatMap(value.y), OrderPreservingFloatMap(value.z));
 }
 
-void WriteBuffer(VFXAttributes attributes, RWStructuredBuffer<uint3> positions, uint index) {    
-	AtomicWriteBuffer(positions, index, attributes.position);
+inline float3 Uint3ToFloat3(float3 value)
+{
+    return float3(InverseOrderPreservingFloatMap(value.x), InverseOrderPreservingFloatMap(value.y), InverseOrderPreservingFloatMap(value.z));
 }
 
-void AtomicLoadBuffer(RWStructuredBuffer<uint3> positions, uint index, out float3 outPosition) {
-    uint3 position = uint3(0, 0, 0);
-    InterlockedAdd(positions[index].x, 0, position.x);
-    InterlockedAdd(positions[index].y, 0, position.y);
-    InterlockedAdd(positions[index].z, 0, position.z);
-    outPosition = float3(InverseOrderPreservingFloatMap(position.x), InverseOrderPreservingFloatMap(position.y), InverseOrderPreservingFloatMap(position.z));    
+void WriteToPositionsBuffer(RWByteAddressBuffer positions, uint index, uint3 inPosition) {
+    uint x,y,z = 0;
+    positions.InterlockedExchange(index*12, inPosition.x, x);
+    positions.InterlockedExchange(index*12+4, inPosition.y, y);
+    positions.InterlockedExchange(index*12+8, inPosition.z, z);
 }
 
-void UpdateRope(inout VFXAttributes attributes, RWStructuredBuffer<uint3> positions, RWStructuredBuffer<uint> mutexes, float targetDist, uint bufferSize, uint currIndex)
-{   
+
+void WriteBuffer(VFXAttributes attributes, RWByteAddressBuffer positions, uint index) {    
+    uint3 position = Float3ToUint3(attributes.position);
+    WriteToPositionsBuffer(positions, index, position);
+}
+
+inline uint3 ReadPositionsBuffer(RWByteAddressBuffer positions, uint index) {
+    uint x,y,z = 0;
+    positions.InterlockedAdd(index * 12, 0, x);
+    positions.InterlockedAdd(index * 12 + 4, 0, y);
+    positions.InterlockedAdd(index * 12 + 8, 0, z);
+    return uint3(x,y,z);
+}
+
+
+void IntegrateVerlet(inout VFXAttributes attributes, float deltaTime)
+{
+    attributes.position = 2 * attributes.position - attributes.oldPosition + ((deltaTime*deltaTime) * attributes.acceleration);
+}
+
+
+void ReadPosition(inout VFXAttributes attributes, RWByteAddressBuffer positions, uint index) {
+    attributes.position = Uint3ToFloat3(ReadPositionsBuffer(positions, index));
+}
+
+void UpdateRopeImpl(inout VFXAttributes attributes, RWByteAddressBuffer positions, RWByteAddressBuffer mutexes, float targetDist, uint bufferSize, uint currIndex)
+{
     uint ignored = 0;
     uint mutex = 0;
-    AtomicWriteBuffer(positions, currIndex, attributes.position);
-    InterlockedExchange(mutexes[currIndex], currIndex + 1, ignored);
+    WriteToPositionsBuffer(positions, currIndex, attributes.position);
+    mutexes.InterlockedExchange(currIndex * 4, currIndex - 1, ignored);
+    {
+        for (uint i = 0; i < 12; ++i) {
+        if (currIndex % 2 == 1) 
+        {   
+            for(uint j=0; j<1000; ++j)
+            {
+                mutexes.InterlockedAdd((currIndex - 1)*4, 0, mutex);
 
-    for (uint i = 0; i < 48; ++i)
-    {   
-        if (currIndex > 0) 
-        {
-            for(uint j=0; j<10; ++j)
-            {
-                InterlockedAdd(mutexes[currIndex - 1], 0, mutex);
-                if(mutex == currIndex)
-                {
-                    float3 current = float3(0.0, 0.0, 0.0);
-                    float3 other = float3(0.0, 0.0, 0.0);
-                    AtomicLoadBuffer(positions, currIndex, current);
-                    // AtomicLoadBuffer(positions, currIndex - 1, other);
-                    
-                    // float3 delta = (current - other);
-                    // float dist = length(delta);
-                    // float halfDist = (dist - targetDist) / 2.0;
-                    // delta = SafeNormalize(delta);
-                    // if (currIndex == bufferSize - 1)
-                    // {
-                    //     AtomicWriteBuffer(positions, currIndex, current + ((halfDist + halfDist) * delta));
-                    // }
-                    // else
-                    // {
-                    //     AtomicWriteBuffer(positions, currIndex, current + (halfDist * delta));
-                    //     AtomicWriteBuffer(positions, currIndex - 1, other - (halfDist * delta));
-                    // }
-                    
-                    InterlockedExchange(mutexes[currIndex - 1], currIndex - 1, ignored);
-                    break;
-                }
-            }
-        }
-        
-        if (currIndex + 1 < bufferSize)
-        {
-            for(uint j=0; j<10; ++j) 
-            {
-                InterlockedAdd(mutexes[currIndex], 0, mutex);
                 if(mutex == currIndex)
                 {
                     // float3 current = float3(0.0, 0.0, 0.0);
                     // float3 other = float3(0.0, 0.0, 0.0);
-                    // AtomicLoadBuffer(positions, currIndex, current);
-                    // AtomicLoadBuffer(positions, currIndex + 1, other);                    
-                    // float3 delta = (current - other);
-                    // float dist = length(delta);
-                    // float halfDist = (dist - targetDist) / 2.0;
-                    // delta = SafeNormalize(delta);
-                  
-                    // AtomicWriteBuffer(positions, currIndex, current + (halfDist * delta));
-                    // AtomicWriteBuffer(positions, currIndex + 1, other - (halfDist * delta));
-                    // InterlockedExchange(mutexes[currIndex], currIndex + 1, ignored);
-                    // break;
+                    float3 current = Uint3ToFloat3(ReadPositionsBuffer(positions, currIndex));
+                    float3 other = Uint3ToFloat3(ReadPositionsBuffer(positions, currIndex - 1));
+                    float3 delta = (current - other);
+                    float dist = length(delta);
+                    float halfDist = (dist - targetDist) / 2.0;
+                    delta = SafeNormalize(delta);
+                    if (currIndex == bufferSize - 1)
+                    {
+                        WriteToPositionsBuffer(positions, currIndex, Float3ToUint3(current + ((halfDist + halfDist) * delta)));
+                    }
+                    else
+                    {
+                        WriteToPositionsBuffer(positions, currIndex, Float3ToUint3(current + (halfDist * delta)));
+                        WriteToPositionsBuffer(positions, currIndex - 1, Float3ToUint3(other - (halfDist * delta)));
+                    }
+
+                    mutexes.InterlockedExchange((currIndex - 1) * 4, currIndex - 2, ignored);
+                    break;
+                }
+            }
+            
+            
+            if (currIndex + 1 < bufferSize)
+            {
+                for(uint k=0; k<1000; ++k) 
+                {
+                    mutexes.InterlockedAdd((currIndex+1) * 4, 0, mutex);
+                    if(mutex == currIndex)
+                    {
+                        float3 current = Uint3ToFloat3(ReadPositionsBuffer(positions, currIndex));
+                        float3 other = Uint3ToFloat3(ReadPositionsBuffer(positions, currIndex + 1));
+                        
+                        float3 delta = (current - other);
+                        float dist = length(delta);
+                        float halfDist = (dist - targetDist) / 2.0;
+                        delta = SafeNormalize(delta);
+                    
+                        WriteToPositionsBuffer(positions, currIndex, current + (halfDist * delta));
+                        WriteToPositionsBuffer(positions, currIndex + 1, other - (halfDist * delta));
+                        mutexes.InterlockedExchange((currIndex + 1) * 4, currIndex + 2, ignored);
+                        break;
+                    }
                 }
             }
         }
     }
+    }
+    attributes.position = Uint3ToFloat3(ReadPositionsBuffer(positions, currIndex));
+}
 
-    AtomicLoadBuffer(positions, currIndex, attributes.position);
+void UpdateRope(inout VFXAttributes attributes, RWByteAddressBuffer positions, RWByteAddressBuffer mutexes, float targetDist, uint bufferSize, uint currIndex)
+{
+    UpdateRopeImpl(attributes, positions, mutexes, targetDist, bufferSize, currIndex);   
 }
 #endif
